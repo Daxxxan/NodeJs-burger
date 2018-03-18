c5b27feefbe5cebd4e3dcdaa2323a048d671383b
Unknown <guenin.vincent4@gmail.com>
2018-03-18 22:02:55 +0100

    Product model definition

---
+++ models/Product.js
@@ -0,0 +1,44 @@
+module.exports = function(sequelize, DataTypes) {
+    const Product = sequelize.define('Product', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            unique: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cal: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        highlight: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        size: {
+            type: DataTypes.CHAR,
+            allowNull: false
+        }
+    }, {
+        paranoid: true,
+        underscored: true,
+        freezeTableName: true
+    });
+    //Product.associate = _associate;
+    return Product;
+};
+
+//TODO Code function _associate
+// INTERNAL FUNCTIONS
+/*
+function _associate(models) {
+
+}
+*/
