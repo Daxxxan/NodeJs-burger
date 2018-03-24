1ac9d7b42a8cd5632d01c031baabb90c87014a67
BENARD Alexis <alex-bad97@hotmail.fr>
2018-03-24 20:30:38 +0100

    setProduct

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,16 @@
 const ProductController = function () {};
+const ModelIndex = require('../models');
+const Product = ModelIndex.Product;
+
+ProductController.setProduct = function(name, cal, highlight, price, size) {
+    return Product.create({
+        name: name,
+        cal: cal,
+        highlight: highlight,
+        price: price,
+        size: size
+    });
+};
 
 ProductController.displayMessage = function () {
     console.log('Page reservée à l\'affichage des produits');
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const controllers = require('../controllers');
+const bodyParser = require('body-parser');
 const ProductController = controllers.ProductController;
 
 const ProductRouter = express.Router();
+ProductRouter.use(bodyParser.json());
 
-ProductRouter.get('/', function (req, res) {
-    ProductController.displayMessage();
-    res.end();
+ProductRouter.post('/', function (req, res) {
+    const name = req.body.name;
+    const cal = req.body.cal;
+    const highlight = req.body.highlight;
+    const price = req.body.price;
+    const size = req.body.size;
+
+    if(name === undefined || cal === undefined || highlight === undefined || price === undefined || size === undefined){
+        res.status(400).end();
+        return;
+    }
+    ProductController.setProduct(name, cal, highlight, price, size)
+    .then((successfullyAdd) => {
+        res.status(201).json(successfullyAdd);
+        res.end();
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+    });
 });
 
 module.exports = ProductRouter;
