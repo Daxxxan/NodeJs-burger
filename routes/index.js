aff3efa318ecb60bd7d1d5a0360026d2bc9a230f
Unknown <guenin.vincent4@gmail.com>
2018-05-14 15:25:17 +0200

    init Swagger doc

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const RouteManager = function() { };
 
 RouteManager.attach = function(app) {
+    app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
     app.use('/product', require('./product'));
     app.use('/menu', require('./menu'));
     app.use('/command', require('./command'));
